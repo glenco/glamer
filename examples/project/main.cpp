e66a7987a7d32025a13253ce30369983fa319d46
Nicolas Tessore <nicolas.tessore@unibo.it>
2013-05-31 23:40:24 +0200

    Example project that uses CMake to find GLAMER settings.

---
+++ examples/project/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// make sure we find headers
+#include "slsimlib.h"
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Hello World" << std::endl;
+	
+	// make sure we link the libraries
+	PixelMap map;
+	
+	return 0;
+}
+
