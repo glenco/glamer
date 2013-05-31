a976f3adf81f0e29fd796b40d59fe6ae359d2162
Nicolas Tessore <nicolas.tessore@unibo.it>
2013-05-31 23:39:37 +0200

    Remove find-modules for GLAMER libraries. Instead, use find_package in config mode to automatically get GLAMER settings into projects. C++11 is now configurable with the USE_CXX11 option. The ENABLE_FITS and ENABLE_FFTW options are set by default.

--- cmake/Modules/GLAMER.cmake
+++ cmake/Modules/GLAMER.cmake
@@ -1,4 +1,53 @@
-# Xcode settings
-set(CMAKE_XCODE_ATTRIBUTE_GCC_C_LANGUAGE_STANDARD "c99")
-set(CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LANGUAGE_STANDARD "c++0x")
-set(CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LIBRARY "libc++")
+####
+# compiler settings
+####
+
+option(USE_CXX11 "Use C++11 for compiling (if supported)." ON)
+
+if(USE_CXX11)
+	if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+		if(CMAKE_GENERATOR STREQUAL "Xcode")
+			# Xcode settings
+			set(CMAKE_XCODE_ATTRIBUTE_GCC_C_LANGUAGE_STANDARD "c99")
+			set(CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LANGUAGE_STANDARD "c++0x")
+			set(CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LIBRARY "libc++")
+			message(STATUS "Using C++11 in Xcode settings.")
+		else()
+			# raw clang++ flags
+			set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -stdlib=libc++")
+			message(STATUS "Using C++11 in clang++ flags.")
+		endif()
+	elseif(MSVC10)
+		message(STATUS "Using C++11 in MSVC10 by default.")
+	else()
+		message(STATUS "Not using C++11, unsupported compiler (${CMAKE_CXX_COMPILER_ID}).")
+	endif()
+endif()
+
+
+####
+# options
+####
+
+# FITS
+option(ENABLE_FITS "Enable FITS support." ON)
+
+if(ENABLE_FITS)
+	find_package(CCfits REQUIRED)
+	add_definitions(-DENABLE_FITS=1)
+	message(STATUS "FITS support: enabled")
+else()
+	message(STATUS "FITS support: disabled")
+endif()
+
+# FFTW
+option(ENABLE_FFTW "Enable functions that require FFTW." ON)
+
+if(ENABLE_FFTW)
+	find_package(FFTW3 REQUIRED)
+	add_definitions(-DENABLE_FFTW=1)
+	message(STATUS "FFTW support: enabled")
+else()
+	message(STATUS "FFTW support: disabled")
+endif()
+
