7d9a2083668697be364b3dd5bf607bef42bd475e
RB Metcalf <bmetcalf@gmail.com>
2020-02-06 17:41:11 +0100

    Made some changes to fix problems with finding cfitsio. 	modified:   CMakeLists.txt 	modified:   cmake/Modules/FindCFITSIO.cmake

--- cmake/Modules/FindCFITSIO.cmake
+++ cmake/Modules/FindCFITSIO.cmake
@@ -6,34 +6,77 @@
 #  CFITSIO_LIBRARIES - The libraries needed to use CFITSIO
 #  CFITSIO_DEFINITIONS - Compiler switches required for using CFITSIO
 
-set(CFITSIO CACHE FILEPATH "Path to CFITSIO.")
+INCLUDE( FindPackageHandleStandardArgs )
 
-find_path(CFITSIO_INCLUDE_DIR
-	fitsio.h
-	HINTS ${CFITSIO}
-	PATH_SUFFIXES "include"
-	DOC "CFITSIO include directory"
+#set(CFITSIO CACHE FILEPATH "Path to CFITSIO.")
+
+IF( DEFINED ENV{CFITSIO_DIR} )
+  SET( CFITSIO "$ENV{CFITSIO_DIR}" )
+ENDIF()
+
+FIND_PATH( CFITSIO_INCLUDE_DIR
+  fitsio.h
+  HINTS ${CFITSIO}
 )
 
-find_library(CFITSIO_LIBRARY
-	NAMES cfitsio libcfitsio
-	HINTS ${CFITSIO}
-	PATH_SUFFIXES "lib"
-	DOC "CFITSIO library"
+FIND_LIBRARY( CFITSIO_LIBRARY
+  NAMES cfitsio
+  HINTS ${CFITSIO}
 )
 
-set(CFITSIO_LIBRARIES ${CFITSIO_LIBRARY})
-set(CFITSIO_INCLUDE_DIRS ${CFITSIO_INCLUDE_DIR})
+FIND_PACKAGE_HANDLE_STANDARD_ARGS( CFITSIO DEFAULT_MSG
+  CFITSIO_INCLUDE_DIR
+  CFITSIO_LIBRARY
+)
 
-include(FindPackageHandleStandardArgs)
+IF( CFITSIO_FOUND )
+  SET( CFITSIO_INCLUDE_DIRS ${CFITSIO_INCLUDE_DIR} )
+  SET( CFITSIO_LIBRARIES ${CFITSIO_LIBRARY} )
+
+  MARK_AS_ADVANCED(
+    CFITSIO_LIBRARY
+    CFITSIO_INCLUDE_DIR
+    CFITSIO_DIR
+  )
+ELSE()
+  SET( CFITSIO_DIR "" CACHE STRING
+    "An optional hint to a directory for finding `cfitsio`"
+  )
+ENDIF()
+
+###############################################################
+
+#find_path(CFITSIO_INCLUDE_DIR
+#	fitsio.h
+#	HINTS ${CFITSIO}
+#	PATHS
+#	  /usr/local/
+#	  /opt/anaconda3/
+#	  /opt/anaconda2/
+#	PATH_SUFFIXES "include"
+#	DOC "CFITSIO include directory"
+#)
+
+#set(CFITSIO_INCLUDE_DIRS ${CFITSIO_INCLUDE_DIR})
+
+#find_library(CFITSIO_LIBRARY
+#	NAMES cfitsio libcfitsio
+#	HINTS ${CFITSIO}
+#	PATH_SUFFIXES "lib"
+#	DOC "CFITSIO library"
+#)
+
+#set(CFITSIO_LIBRARIES ${CFITSIO_LIBRARY})
+
+#include(FindPackageHandleStandardArgs)
 
 # handle the QUIETLY and REQUIRED arguments and set CFITSIO_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(
-	CFITSIO
-	DEFAULT_MSG
-	CFITSIO_LIBRARY
-	CFITSIO_INCLUDE_DIR
-)
+#find_package_handle_standard_args(
+#	CFITSIO
+#	DEFAULT_MSG
+#	CFITSIO_LIBRARY
+#	CFITSIO_INCLUDE_DIR
+#)
 
-mark_as_advanced(CFITSIO_INCLUDE_DIR CFITSIO_LIBRARY)
+#mark_as_advanced(CFITSIO_INCLUDE_DIR CFITSIO_LIBRARY)
